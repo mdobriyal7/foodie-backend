4a2d63ed3a5dd91750fef3265686ac7831215f8e
Manish <mdobriyal7@gmail.com>
2023-07-13 16:51:26 +0530

    minor change

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,10 +1,8 @@
 const cloudinary = require("cloudinary").v2;;
 
-console.log(process.env.CLOUDINARY_API_KEY)
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:688331351542189,
+  api_key:process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
